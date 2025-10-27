a74cae0d35c5fe817adc823165100b5be8d69129
Predator163 <sumanthkumarsoldier@gmail.com>
2025-10-28 02:30:00 +0530

    Add Vercel URL to Django CORS settings

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -64,9 +64,30 @@ MIDDLEWARE = [
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000",
     "https://fake-job-backend.onrender.com",
+    "https://fake-job-detection-henna.vercel.app",
     # Add your Vercel/Render frontend URL here later
 ]  # For development only
 CORS_ALLOW_CREDENTIALS = True
+
+CORS_ALLOW_METHODS = [
+    'DELETE',
+    'GET',
+    'OPTIONS',
+    'PATCH',
+    'POST',
+    'PUT',
+]
+CORS_ALLOW_HEADERS = [
+    'accept',
+    'accept-encoding',
+    'authorization',
+    'content-type',
+    'dnt',
+    'origin',
+    'user-agent',
+    'x-csrftoken',
+    'x-requested-with',
+]
 
 REST_FRAMEWORK = {
     'DEFAULT_RENDERER_CLASSES': [
